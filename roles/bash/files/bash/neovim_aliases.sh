794667e3f7954169e53b18addc246ce91d3f8784
Edvin Lidholm <edvin.lidholm@volvocars.com>
2024-10-14 08:08:13 +0200

    feat: create search to nvim alias

--- roles/bash/files/bash/neovim_aliases.sh
+++ roles/bash/files/bash/neovim_aliases.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-alias v="nvim"
+alias v="fdfind --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs nvim"
 alias vi="nvim"
 alias vim="nvim"
-alias ni="nvim"
 alias svi="sudo nvim"
 alias vis='nvim "+set si"'
 alias nvimsetup="cd \$HOME/.config/nvim"
-alias vimsetup="cd \$HOME/.config/nvim"
 
 #config
 alias bconf="nvim ~/.bashrc"
+alias iconf="nvim ~/.inputrc"
 alias nconf="nvim ~/.config/nvim"
 alias tconf="nvim ~/.tmux.conf"
 alias aconf="nvim ~/.config/alacritty/alacritty.toml"
-
