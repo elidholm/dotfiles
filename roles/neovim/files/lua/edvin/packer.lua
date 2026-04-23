30c96bb2fb35b0367dd53fa2c0634dff4bc073d9
Edvin Lidholm <edvin.lidholm@volvocars.com>
2026-04-23 14:18:30 +0200

    (nvim): remove refactoring and treesitter-playground plugins

--- roles/neovim/files/lua/edvin/packer.lua
+++ roles/neovim/files/lua/edvin/packer.lua
@@ -34,9 +34,7 @@ return require("packer").startup(function(use)
         end
     })
 
-    use("nvim-treesitter/playground")
     use("theprimeagen/harpoon")
-    use("theprimeagen/refactoring.nvim")
     use("mbbill/undotree")
     use("tpope/vim-fugitive")
     use("nvim-treesitter/nvim-treesitter-context");
