ad743ac07a07e25bbe0aa37b968e79f34a3a3d78
Edvin Lidholm <edvin.lidholm@volvocars.com>
2026-04-24 15:28:35 +0200

    Add 'telescope' and 'treesitter' plugins

--- roles/neovim/files/lua/config/lazy/colors.lua
+++ roles/neovim/files/lua/config/lazy/colors.lua
@@ -10,9 +10,13 @@ end
 return {
     {
         "catppuccin/nvim",
-        lazy = false,
         priority = 1000,
         config = function() 
+            require("catppuccin").setup({
+                flavour = "mocha",
+                transparent_background = true,
+            })
+
             ColorMyPencils()
         end,
     },
