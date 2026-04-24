965053f2a9e9277a79b0d5a60e1501b41f44007d
Edvin Lidholm <edvin.lidholm@volvocars.com>
2026-04-24 15:47:18 +0200

    Add 'autopairs' and 'lazygit' Neovim plugins

--- roles/neovim/files/lua/config/lazy/colors.lua
+++ roles/neovim/files/lua/config/lazy/colors.lua
@@ -8,16 +8,14 @@ function ColorMyPencils(color)
 end
 
 return {
-    {
-        "catppuccin/nvim",
-        priority = 1000,
-        config = function() 
-            require("catppuccin").setup({
-                flavour = "mocha",
-                transparent_background = true,
-            })
+    "catppuccin/nvim",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "mocha",
+            transparent_background = true,
+        })
 
-            ColorMyPencils()
-        end,
-    },
+        ColorMyPencils()
+    end,
 }
