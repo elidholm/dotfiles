26461185a9baea1ec1acdf5ffa8f4787e1a28812
Edvin Lidholm <edvin.lidholm@volvocars.com>
2026-06-02 09:55:17 +0200

    refact(nvim): apply subset of kickstart.nvim options

--- roles/neovim/files/lua/config/set.lua
+++ roles/neovim/files/lua/config/set.lua
@@ -1,31 +1,47 @@
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.api.nvim_command("filetype plugin indent on")
+vim.opt.mouse = 'a'
+vim.opt.showmode = false
+
+vim.opt.wrap = false
+vim.opt.smartindent = true
+
+vim.api.nvim_command('filetype plugin indent on')
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
-vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+vim.opt.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20'
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
 
-vim.opt.updatetime = 50
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
-vim.opt.colorcolumn = "120"
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.opt.scrolloff = 10
+vim.opt.signcolumn = 'yes'
+vim.opt.cursorline = true
+
+vim.opt.colorcolumn = '120'
+vim.opt.isfname:append('@-@')
+
+vim.opt.inccommand = 'split'
+
+vim.opt.confirm = true
